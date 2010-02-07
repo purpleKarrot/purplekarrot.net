c6194f0c6231350fb9f7934156ab2a34b857cf20
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-02-07 18:44:54 +0100

    tidy css

---
+++ CMake/AddCSS.cmake
@@ -0,0 +1,32 @@
+
+find_program(SASS_EXECUTABLE sass)
+find_program(CSSTIDY_EXECUTABLE csstidy)
+set(CSSTIDY_PARAMETERS --silent=true --template=highest)
+
+macro(add_css OUTPUT)
+
+  get_filename_component(NAME ${OUTPUT} NAME_WE)
+  set(SASS_FILE ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.sass)
+  set(MESS_FILE ${CMAKE_CURRENT_BINARY_DIR}/${NAME}-raw.css)
+
+  set(DEPENDENCIES)
+  file(REMOVE ${SASS_FILE})
+  
+  foreach(DEPENDENCY ${ARGN})
+    set(DEPENDENCIES ${DEPENDENCIES} ${DEPENDENCY})
+    file(APPEND ${SASS_FILE} "@import ../${DEPENDENCY}\n")
+  endforeach(DEPENDENCY ${ARGN})
+
+  add_custom_command(OUTPUT ${MESS_FILE}
+    COMMAND ${SASS_EXECUTABLE} ${SASS_FILE} ${MESS_FILE}
+    DEPENDS ${DEPENDENCIES}
+    )
+
+  add_custom_command(OUTPUT ${OUTPUT}
+    COMMAND ${CSSTIDY_EXECUTABLE} ${MESS_FILE} ${CSSTIDY_PARAMETERS} ${OUTPUT}
+    DEPENDS ${MESS_FILE}
+    )
+
+  add_custom_target(${NAME}-css ALL DEPENDS ${OUTPUT})
+
+endmacro(add_css OUTPUT)
