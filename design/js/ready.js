85bdff920906137b3dde0e7277d6d9c01c53dc70
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-07-31 22:11:19 +0200

    it is moving!

---
+++ design/js/ready.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+	$('.tagcanvas').tagcanvas(
+		{
+		textColour : '#65944A',
+		outlineColour: '#555555',
+		outlineThickness : 1,
+		reverse : true
+		}
+	);
+	// your other jQuery stuff here...
+});
+
